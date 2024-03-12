f5b542c03c050899c45646d285fbee81ee904114
Poul Nichols <poulnichols@gmail.com>
2024-03-12 08:00:29 -0700

    initial

---
+++ scripts/myscripts.js
@@ -0,0 +1,12 @@
+// myscripts.js
+'use strict'
+
+// eslint-disable-next-line no-unused-vars
+function showAlt (x) {
+  document.getElementById('alttext').innerHTML = x.alt
+}
+
+// eslint-disable-next-line no-unused-vars
+function hideAlt () {
+  document.getElementById('alttext').innerHTML = ''
+}
